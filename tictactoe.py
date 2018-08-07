03b440a9007b6c7bea291beb1d2a3af046ad6bf3
Abdi Mohamud <amohamud23@gmail.com>
2018-08-06 23:56:15 -0700

    completed makeMove function

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,25 @@
-l = list()
-for i in range(3): print line
+
+board =[['','',''],['','',''],['','','']]
+
+
+def drawBoard(board):
+    print("-----------------")
+    for i in board:
     
+        for j in i:
+            print(" ",j,end=' |')
+    
+        print()
+        print("-----------------")
+    return
+
+
+
+def makeMove(board):
+   row = int(input("Enter Row number (0,1,2): "))
+   col = int(input("Enter Column number (0,1,2): "))
+   board[row][col] = 'X'
+   drawBoard(board)
+
+drawBoard(board)
+makeMove(board)
