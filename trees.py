edc42854e8de60e8b37b89c5fa0d52e8210b10eb
Abdi <amohamud23@gmail.com>
2019-04-17 11:45:08 -0700

    create node objects with values and a traverse method

--- trees.py
+++ trees.py
@@ -7,10 +7,36 @@ class Node(object):
 
 
 root = Node()
-root.data = "root"
-root.left = Tree()
-root.left = "left"
-root.right = Tree()
-root.right = "right"
+n1 = Node()
+n2 = Node()
+n3 = Node()
+n4 = Node()
+n5 = Node()
+n6 = Node()
+root.data = 1
+root.left = n1
+root.right = n2
 
-print()
+n1.data = 1
+n2.data = 2
+
+n1.left = n3
+n1.right = n4
+
+n3.data = 3
+n4.data = 4
+
+n2.left = n5
+n2.right = n6
+
+n5.data = 5
+n6.data = 6
+
+def traverse(root):
+    if root != None:
+        print(root.data)
+        traverse(root.left)
+        traverse(root.right)
+
+traverse(root)
+
